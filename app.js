c39a89dd377b596165ed11b6805fccde7f538894
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-01-17 22:04:29 -0500

    day one homework

---
+++ app.js
@@ -0,0 +1,6 @@
+let main = document.querySelector("main");
+let trunchbull = () => {
+  main.innerHTML =
+    "<img src='https://www.looper.com/img/gallery/why-agatha-trunchbull-from-matilda-looks-so-familiar/l-intro-1621261212.jpg'>";
+};
+document.querySelector("button").onclick = trunchbull;
